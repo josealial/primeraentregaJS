d756a8a4386ca46ab5429b389cc1e102ec5c64ab
josealial <130402401+josealial@users.noreply.github.com>
2023-06-03 21:30:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function obtenerPrecioDestino(destino) {
+    switch (destino) {
+        case "montevideo":
+            return "El pasaje a Montevideo cuesta 30 Dolares"
+        case "colonia":
+            return "El pasaje a Colonia cuesta 50 Dolares"
+        case "rocha":
+            return "El pasaje a Rocha cuesta 60 Dolares"
+        default:
+            return "De momento no contamos con ese destino"
+    }
+}
+
+let respuesta
+
+while (true) {
+    respuesta = prompt("Ingrese el destino para saber su precio en minúscula \nMontevideo\nColonia\nRocha\nPara cancelar ingrese: salir")
+
+    if (respuesta === "salir") {
+
+        break
+    }
+
+    alert(obtenerPrecioDestino(respuesta))
+}
